13219b7b644b93ea47961b4dabe3614e02679ae0
Simon Haines <nimon@hotmail.co.uk>
2025-10-15 10:10:49 +0100

    fix:recipe book property name

--- src/main/java/com/corndel/cashregister/exercises/Exercise1b.java
+++ src/main/java/com/corndel/cashregister/exercises/Exercise1b.java
@@ -4,10 +4,10 @@ import com.corndel.cashregister.exercises.models.RecipeBook;
 
 public class Exercise1b {
   /**
-   * This function changes the recipeBook's name to the newName and then returns
+   * This function changes the recipeBook's title to the newTitle and then returns
    * the updated recipeBook
    */
-  public static RecipeBook renameBook(RecipeBook recipeBook, String newName) {
+  public static RecipeBook renameBook(RecipeBook recipeBook, String newTitle) {
     // TODO
     return null;
   }
